00ffdcd17976ebb9968b1568b4ffb0d2604c65e8
yellowsea <yunhai0644@163.com>
2022-09-27 23:07:41 +0800

    update

--- 35.搜索插入位置.ts
+++ 35.搜索插入位置.ts
@@ -8,14 +8,21 @@
 function searchInsert(nums: number[], target: number): number {
   let res = nums.indexOf(target)
   if (res !== -1) {
+    // 如果找到了，直接返回
     return res
   } else {
     // res == -1
+    // nums 里面没有 target时
+
+
+    // 1. 当 target 比 nums 中任何一个小时，返回 比 target 大的那个数的索引
     for (let i = 0; i < nums.length; i++) {
       if (nums[i] > target) {
         return i
       }
     }
+
+    // 2. 当 target 比 nums 中所有的数都大时，返回 nums.length
     if (nums[nums.length - 1] < target) {
       return nums.length
     }
