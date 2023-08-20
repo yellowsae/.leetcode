deb3b17a7666a66003a53a1df20d2784c0dd5e14
yellowsea <yunhai0644@163.com>
2023-08-20 20:56:34 +0800

    chore: update

--- 141.环形链表.ts
+++ 141.环形链表.ts
@@ -17,21 +17,43 @@
  * }
  */
 
+// 双指针
+// function hasCycle(head: ListNode | null): boolean {
+//   // 定义一个快指针和一个慢指针
+//   let p1 = head
+//   let p2 = head
+//   while (p1 && p2 && p2.next) {
+//     // 移动指针 
+//     p1 = p1.next
+//     p2 = p2.next.next
+
+//     if (p1 === p2) {
+//       return true
+//     }
+//   }
+
+//   return false
+// };
+
+
+
+// 使用 Set 
 function hasCycle(head: ListNode | null): boolean {
-  // 定义一个快指针和一个慢指针
-  let p1 = head
-  let p2 = head
-  while (p1 && p2 && p2.next) {
-    // 移动指针 
-    p1 = p1.next
-    p2 = p2.next.next
+  if (head === null) return false
+  let set = new Set()
 
-    if (p1 === p2) {
+  while (head) {
+
+    // 判断是否 出现过 
+    if (set.has(head)) {
       return true
+    } else {
+      set.add(head)
     }
+    head = head.next
   }
-
   return false
 };
+
 // @lc code=end
 
